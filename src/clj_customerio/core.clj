cbb9bc6cebc977717a30cfadafcd4c3a4eb19ef2
chrisclark <chris@untrod.com>
2014-10-20 10:52:48 -0700

    more scoping

--- src/clj_customerio/core.clj
+++ src/clj_customerio/core.clj
@@ -6,6 +6,16 @@
             [clojure.tools.cli :refer [cli]]
             [clojure.data.json :as json]))
 
+(defn build-req
+  [obj opts]
+  {:headers {"Content-Type" "application/json"}
+   :basic-auth [(:user opts) (:password opts)]
+   :body (json/write-str obj)})
+
+(defn path
+  [customer]
+  (str "https://track.customer.io/api/v1/customers/" (:id customer)))
+
 (defn -main
   [& args]
   (let [[opts args banner] (cli args
@@ -14,19 +24,9 @@
                                 ["-p" "--password" "customer.io API Key" :flag false])
         emails (read-customers (:file opts))]
 
-    (defn build-req
-      [obj]
-      {:headers {"Content-Type" "application/json"}
-       :basic-auth [(:user opts) (:password opts)]
-       :body (json/write-str obj)})
-
-    (defn path
-      [customer]
-      (str "https://track.customer.io/api/v1/customers/" (:id customer)))
-
     (doseq [customer emails]
       (let [{:keys [status headers body error] :as resp}
-            @(http/put (path customer) (build-req customer))]
+            @(http/put (path customer) (build-req customer opts))]
         (if (= 200 status)
           (println "Successfully upserted" (:email customer))
           (println "Failed: " (str body error)))))))
